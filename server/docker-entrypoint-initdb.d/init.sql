f637f2c4708a4b9b9d1a84fa42d07c08d1317966
Lucasna28 <U2451146@rts-365.dk>
2025-01-06 12:22:49 +0100

    tables

--- server/docker-entrypoint-initdb.d/init.sql
+++ server/docker-entrypoint-initdb.d/init.sql
@@ -1,4 +1,15 @@
-// filepath: /Users/lucasanderson/Documents/skole/WUHF/Serverside/lolan/init.sql
+SELECT 'hello world...' as '';
+
+DROP TABLE IF EXISTS likes;
+SELECT 'Dropped likes table...' as '';
+
+DROP TABLE IF EXISTS posts;
+SELECT 'Dropped posts table...' as '';
+
+DROP TABLE IF EXISTS users;
+SELECT 'Dropped users table...' as '';
+
+-- Opret users tabel
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL UNIQUE,
@@ -11,13 +22,15 @@ CREATE TABLE users (
     website VARCHAR(100) NOT NULL,
     followers INT DEFAULT 0,
     following INT DEFAULT 0,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+SELECT 'Created users table...' as '';
 
+-- Opret posts tabel
 CREATE TABLE posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT UNIQUE,
+    user_id INT,
     content VARCHAR(255),
     image VARCHAR(255),
     video VARCHAR(255),
@@ -27,11 +40,16 @@ CREATE TABLE posts (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+SELECT 'Created posts table...' as '';
 
+-- Opret likes tabel
 CREATE TABLE likes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
-    tweet_id INT,
+    post_id INT,
     FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (tweet_id) REFERENCES tweets(id)
+    FOREIGN KEY (post_id) REFERENCES posts(id)
 );
+SELECT 'Created likes table...' as '';
+
+SELECT 'Database initialization completed!' as '';
