1e42ea9dbcd05cf7349bc4303aa5e6ecb38deab7
agerskov <jokkexx7@gmail.com>
2025-01-07 12:09:54 +0100

    changed get POST to GET

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,23 +3,23 @@
 import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    const fetcher = async () => {
-      try {
-        const res = await fetch("/api/getMetodes/getPosts.php");
-        if (!res.ok) {
-          console.log("API fejl:", res.status);
-          return;
-        }
-        const data = await res.json();
-        console.log("API svar:", data);
-      } catch (err) {
-        console.error("Fetch fejl:", err);
-      }
-    };
+    useEffect(() => {
+        const fetcher = async () => {
+            try {
+                const res = await fetch("/api/posts/getPost/index.php?id=1");
+                if (!res.ok) {
+                    console.log("API fejl:", res.status);
+                    return;
+                }
+                const data = await res.json();
+                console.log("API svar:", data);
+            } catch (err) {
+                console.error("Fetch fejl:", err);
+            }
+        };
 
-    fetcher();
-  }, []);
+        fetcher();
+    }, []);
 
-  return <h1>hellodsaod</h1>;
+    return <h1>hellodsaod</h1>;
 }
